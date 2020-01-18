5eace5cc51e90d81f9abcbc241063379696c1493
gksmfwm00 <gksmfwm00@naver.com>
2020-01-19 02:10:38 +0900

    Create 15657.java

---
+++ Main_백준_15657_N과M8.java
@@ -0,0 +1,50 @@
+package Backtracking;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_백준_15657_N과M8 {
+	private static int[] data;
+	private static int[] res;
+	private static StringBuilder sb;
+	
+	private static void homogeneous(int n, int m, int depth, int start){
+		if(depth == m){
+			for(int i=0;i<m;i++)
+				sb.append(res[i] + " ");
+			
+			sb.append("\n");
+			
+			return;
+		}
+		
+		for(int i=start;i<n;i++){
+			res[depth] = data[i];
+			homogeneous(n, m, depth + 1 , i);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		data = new int[N];
+		res = new int[M];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0;i<N;i++)
+			data[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(data);
+		
+		sb = new StringBuilder();
+		homogeneous(N, M, 0, 0);
+
+		System.out.print(sb.toString());
+	}
+}
