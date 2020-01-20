259a9ccb7bbf430fe39cabe1acff072984d150dc
gksmfwm00 <59398699+gksmfwm00@users.noreply.github.com>
2020-01-20 19:03:38 +0900

    Create 2743.java

---
+++ Main_백준_2743_단어길이재기.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_백준_2743_단어길이재기 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		
+		System.out.print(line.length());
+	}
+}
