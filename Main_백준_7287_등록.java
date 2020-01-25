4852b5cce6894add820f097322ad5836313e9d67
gksmfwm00 <gksmfwm00@naver.com>
2020-01-25 17:35:23 +0900

    Create 7287.java

---
+++ Main_백준_7287_등록.java
@@ -0,0 +1,9 @@
+
+public class Main_백준_7287_등록 {
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		sb.append("303\n");
+		sb.append("gksmfwm1000");
+		System.out.print(sb.toString());
+	}
+}
