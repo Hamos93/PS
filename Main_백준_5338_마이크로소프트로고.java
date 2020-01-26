66f1f983657e5157951c53cfec38331995995992
gksmfwm00 <gksmfwm00@naver.com>
2020-01-26 17:08:54 +0900

    Create 5338.java

---
+++ Main_백준_5338_마이크로소프트로고.java
@@ -0,0 +1,12 @@
+public class Main_백준_5338_마이크로소프트로고 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("       _.-;;-._\n");
+		sb.append("'-..-'|   ||   |\n");
+		sb.append("'-..-'|_.-;;-._|\n");
+		sb.append("'-..-'|   ||   |\n");
+		sb.append("'-..-'|_.-''-._|");
+		
+		System.out.print(sb.toString());
+	}
+}
