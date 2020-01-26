ba1f27ac00e7cd2bebad86acf56b6f428e4f0571
gksmfwm00 <gksmfwm00@naver.com>
2020-01-27 01:59:02 +0900

    Create 16394.java

---
+++ Main_백준_16394_홍익대학교.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main_백준_16394_홍익대학교 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		System.out.print(year - 1946);
+	}
+}
