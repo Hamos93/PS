7bccbd400866126581d6f349b398408c6779d5b6
gksmfwm00 <gksmfwm00@naver.com>
2020-01-26 17:15:03 +0900

    Create 5339.java

---
+++ Main_백준_5339_콜센터.java
@@ -0,0 +1,17 @@
+public class Main_백준_5339_콜센터 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("     /~\\\n");
+		sb.append("    ( oo|\n");
+		sb.append("    _\\=/_\n");
+		sb.append("   /  _  \\\n");
+		sb.append("  //|/.\\|\\\\ \n");
+		sb.append(" ||  \\ /  ||\n");
+		sb.append("============\n");
+		sb.append("|          |\n");
+		sb.append("|          |\n");
+		sb.append("|          |");
+		
+		System.out.print(sb.toString());
+	}
+}
