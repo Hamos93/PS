462454d3ebd7078c59b7086e59760fe389b2d30c
gksmfwm00 <gksmfwm00@naver.com>
2020-01-28 00:55:50 +0900

    Create 10821.java

---
+++ Main_백준_10821_정수의개수.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_백준_10821_정수의개수 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] S = br.readLine().split(",");
+		System.out.print(S.length);
+	}
+}
