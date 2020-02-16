d3cd293d3aa3a5ecfdcb94035c999729b7d980fe
gksmfwm00 <gksmfwm00@naver.com>
2020-02-16 18:35:35 +0900

    Create 2558.java

---
+++ Main_백준_2558_AB_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main_백준_2558_AB_2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.print(A + B);
+	}
+}
