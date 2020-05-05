534f4f0f507ada4a83930570fe340c04e49e2379
gksmfwm00 <gksmfwm00@naver.com>
2020-05-06 00:33:52 +0900

    Create D1_2025

---
+++ SWEA/D1/Solution_D1_2025_N줄덧셈.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D1_2025_N¡Ÿµ°º¿ {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		int answer = 0;
+		for(int n=1;n<=N;n++) answer += n;
+		
+		System.out.print(answer);
+	}
+}
