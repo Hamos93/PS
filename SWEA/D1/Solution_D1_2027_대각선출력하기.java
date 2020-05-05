56ae01efde73701b5d820a22c00ad7edf0239f61
gksmfwm00 <gksmfwm00@naver.com>
2020-05-06 00:31:10 +0900

    Create D1_2027

---
+++ SWEA/D1/Solution_D1_2027_대각선출력하기.java
@@ -0,0 +1,13 @@
+
+public class Solution_D1_2027_대각선출력하기 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("#++++\n");
+		sb.append("+#+++\n");
+		sb.append("++#++\n");
+		sb.append("+++#+\n");
+		sb.append("++++#");
+		
+		System.out.print(sb.toString());
+	}	
+}
