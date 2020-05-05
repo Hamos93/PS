30a405f3c152adab7ebf7cc1f5fee200e94385f8
gksmfwm00 <gksmfwm00@naver.com>
2020-05-05 19:05:08 +0900

    Create D1_2046

---
+++ SWEA/D1/Solution_D1_2046_스탬프찍기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D1_2046_½ºÅÆÇÁÂï±â {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int sharpOfNumber = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		while(0 < sharpOfNumber--) sb.append("#");
+		
+		System.out.print(sb.toString());
+	}
+}
