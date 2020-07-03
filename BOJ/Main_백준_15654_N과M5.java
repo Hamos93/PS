094761b8fb13407cc6306b27b683a006d801f61d
gksmfwm00 <gksmfwm00@naver.com>
2020-07-03 14:58:27 +0900

    순열과 조합

--- BOJ/Main_백준_15654_N과M5.java
+++ BOJ/Main_백준_15654_N과M5.java
@@ -1,5 +1,3 @@
-package Backtracking;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -11,17 +9,20 @@ public class Main_백준_15654_N과M5 {
 	private static boolean[] visit;
 	private static StringBuilder sb;
 	
-	private static void permutation(int n, int m, int depth){
-		if(depth == m){
-			for(int i=0;i<m;i++)
-				sb.append(res[i] + " ");
+	private static void permutation(int n, int m, int depth) {
+		if(depth == m) {
+			for(int i=0;i<m;i++) {
+				if(i == m-1) sb.append(res[i]);
+				else sb.append(res[i] + " ");
+			}
+			
 			sb.append("\n");
 			
 			return;
 		}
 		
-		for(int i=0;i<n;i++){
-			if(!visit[i]){
+		for(int i=0;i<n;i++) {
+			if(!visit[i]) {
 				visit[i] = true;
 				res[depth] = data[i];
 				
@@ -35,23 +36,23 @@ public class Main_백준_15654_N과M5 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 	
 		data = new int[N];
-		res = new int[M];
-		visit = new boolean[N];
 		
 		st = new StringTokenizer(br.readLine(), " ");
 		for(int i=0;i<N;i++)
 			data[i] = Integer.parseInt(st.nextToken());
 		
 		Arrays.sort(data);
-
-		sb = new StringBuilder();
-		permutation(N, M, 0);
+		
+		res = new int[M];
+		visit = new boolean[N];
 	
+		permutation(N, M, 0);
 		System.out.print(sb.toString());
 	}
 }
