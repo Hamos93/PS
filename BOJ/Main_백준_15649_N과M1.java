2a1cd2b78e21b621fd508f731b33a0a747c7f265
gksmfwm00 <gksmfwm00@naver.com>
2020-07-03 13:56:58 +0900

    순열과 조합

--- BOJ/Main_백준_15649_N과M1.java
+++ BOJ/Main_백준_15649_N과M1.java
@@ -1,5 +1,3 @@
-package Backtracking;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -8,44 +6,49 @@ public class Main_백준_15649_N과M1 {
 	private static int[] data;
 	private static int[] res;
 	private static boolean[] visit;
+	private static StringBuilder sb;
 	
-	private static void permutation(int n, int m, int depth){
-		if(depth == m){
-			StringBuilder sb = new StringBuilder();
-			for(int i=0;i<m;i++)
-				sb.append(res[i] + " ");
+	private static void permutation(int n, int m, int depth) {
+		if(depth == m) {
+			for(int i=0;i<m;i++) {
+				if(i == m-1) sb.append(res[i]);
+				else sb.append(res[i] + " ");
+			}
+			
 			sb.append("\n");
 			
-			System.out.print(sb.toString());
 			return;
 		}
 		
-		for(int i=0;i<n;i++){
-			if(!visit[i]){
+		for(int i=0;i<n;i++) {
+			if(!visit[i]) {
 				visit[i] = true;
 				res[depth] = data[i];
 				
 				permutation(n, m, depth + 1);
 				
 				visit[i] = false;
-			} 
+			}
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
 		data = new int[N];
-		res = new int[M];
-		visit = new boolean[N];
-		
 		for(int i=0;i<N;i++)
 			data[i] = i+1;
 		
+		res = new int[M];
+		visit = new boolean[N];
+		
 		permutation(N, M, 0);
+	
+		System.out.print(sb.toString());
 	}
 }
