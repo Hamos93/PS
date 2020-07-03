7af63bdc4e5d8c5ac6017414ab23930c57f645a1
gksmfwm00 <gksmfwm00@naver.com>
2020-07-03 14:08:51 +0900

    순열과 조합

--- BOJ/Main_백준_15651_N과M3.java
+++ BOJ/Main_백준_15651_N과M3.java
@@ -1,5 +1,3 @@
-package Backtracking;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -9,16 +7,19 @@ public class Main_백준_15651_N과M3 {
 	private static int[] res;
 	private static StringBuilder sb;
 	
-	private static void product(int n, int m, int depth){
-		if(depth == m){
-			for(int i=0;i<m;i++)
-				sb.append(res[i] + " ");
+	private static void product(int n, int m, int depth) {
+		if(depth == m) {
+			for(int i=0;i<m;i++) {
+				if(i == m-1) sb.append(res[i]);
+				else sb.append(res[i] + " ");
+			}
+			
 			sb.append("\n");
 			
 			return;
 		}
 		
-		for(int i=0;i<n;i++){
+		for(int i=0;i<n;i++) {
 			res[depth] = data[i];
 			product(n, m, depth + 1);
 		}
@@ -27,17 +28,17 @@ public class Main_백준_15651_N과M3 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 	
 		data = new int[N];
+		for(int i=0;i<N;i++)
+			data[i] = i + 1;
+		
 		res = new int[M];
 		
-		for(int i=0;i<N;i++)
-			data[i] = i+1;
-
-		sb = new StringBuilder();
 		product(N, M, 0);
 		
 		System.out.print(sb.toString());
