0b9cbd7cd31b56e5d42c58e04d21cd108620638e
gksmfwm00 <gksmfwm00@naver.com>
2020-07-03 14:03:17 +0900

    순열과 조합

--- BOJ/Main_백준_15650_N과M2.java
+++ BOJ/Main_백준_15650_N과M2.java
@@ -1,5 +1,3 @@
-package Backtracking;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -8,21 +6,22 @@ public class Main_백준_15650_N과M2 {
 	private static int[] data;
 	private static int[] res;
 	private static boolean[] visit;
+	private static StringBuilder sb;
 	
-	private static void combination(int n, int m, int depth, int start){
-		if(depth == m){
-			StringBuilder sb = new StringBuilder();
-			for(int i=0;i<m;i++)
-				sb.append(res[i] + " ");
-			sb.append("\n");
+	private static void combination(int n, int m, int depth, int start) {
+		if(depth == m) {
+			for(int i=0;i<m;i++) {
+				if(i == m-1) sb.append(res[i]);
+				else sb.append(res[i] + " ");
+			}
 			
-			System.out.print(sb.toString());
+			sb.append("\n");
 			
 			return;
 		}
 		
-		for(int i=start;i<n;i++){
-			if(!visit[i]){
+		for(int i=start;i<n;i++) {
+			if(!visit[i]) {
 				visit[i] = true;
 				res[depth] = data[i];
 				
@@ -36,17 +35,19 @@ public class Main_백준_15650_N과M2 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
+	
 		data = new int[N];
+		for(int i=0;i<N;i++)
+			data[i] = i + 1;
+		
 		res = new int[M];
 		visit = new boolean[N];
 		
-		for(int i=0;i<N;i++)
-			data[i] = i+1;
-		
 		combination(N, M, 0, 0);
+		System.out.print(sb.toString());
 	}
 }
