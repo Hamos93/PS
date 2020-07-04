dd0df56335d9d8f2d788682998ea3bcd77da45b7
gksmfwm00 <gksmfwm00@naver.com>
2020-07-05 01:38:51 +0900

    Create 2581

--- BOJ/Main_백준_2581_소수.java
+++ BOJ/Main_백준_2581_소수.java
@@ -2,36 +2,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main_백준_2581_소수 {
-	private static boolean isPrime(int num){
-		if(num == 1) return false;
-		
-		boolean flag = true;
-		for(int i=2;i<=num-1;i++)
-			if(num % i == 0) flag = false;
-		
-		return flag;
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-	
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+
 		List<Integer> list = new ArrayList<>();
-		for(int i=M;i<=N;i++)
-			if(isPrime(i)) list.add(i);
-		
-		if(list.size() == 0) {
-			System.out.print("-1");
-			return;
+		for(int i=M;i<=N;i++) {
+			if(i == 1) continue;
+
+			int num = i;
+
+			boolean flag = true;
+			for(int j=2;j<=num/2;j++) {
+				if(num % j == 0) {
+					flag = false;
+					break;
+				}
+			}
+
+			if(flag) list.add(num);
 		}
-	
-		int sum = 0;
-		for(int i=0;i<list.size();i++)
-			sum += list.get(i);
-		
-		System.out.print(sum + "\n" + list.get(0));
+
+		if(list.size() != 0) {
+			int sum = 0;
+			for(int i=0;i<list.size();i++)
+				sum += list.get(i);
+
+			System.out.print(sum + "\n" + list.get(0));
+		}else System.out.print("-1");
 	}
 }
