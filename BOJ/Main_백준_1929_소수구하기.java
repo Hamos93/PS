c7d1fef1da8c9c83aba6bd80dfe993a753e499fa
gksmfwm00 <gksmfwm00@naver.com>
2020-07-05 19:02:34 +0900

    에라토스테네스의 체

--- BOJ/Main_백준_1929_소수구하기.java
+++ BOJ/Main_백준_1929_소수구하기.java
@@ -9,18 +9,22 @@ public class Main_백준_1929_소수구하기 {
 		
 		int M = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
-		
+
 		boolean[] prime = new boolean[N+1];
-		prime[0] = prime[1] = true;
+		prime[0] = true;
+		prime[1] = true;
 		
-		for(int i=2;i<=N;i++){
-			for(int j=i+i;j<=N;j+=i)
-				prime[j] = true;
+		for(int i=2;i<=N;i++) {
+			if(!prime[i]) {
+				for(int j=i+i;j<=N;j+=i)
+					prime[j] = true;
+			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i=M;i<=N;i++)
+		for(int i=M;i<=N;i++) {
 			if(!prime[i]) sb.append(i + "\n");
+		}
 		
 		System.out.print(sb.toString());
 	}
