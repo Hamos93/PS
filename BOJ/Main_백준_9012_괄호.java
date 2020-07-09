43f833c458274b8a3291ab86559251ae4937923d
gksmfwm00 <gksmfwm00@naver.com>
2020-07-10 01:42:04 +0900

    Create 9012

--- BOJ/Main_백준_9012_괄호.java
+++ BOJ/Main_백준_9012_괄호.java
@@ -1,27 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main_백준_9012_괄호 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		while(T-- > 0){
-			Stack<Character> stack = new Stack<>();
-
+		int T = Integer.parseInt(st.nextToken());
+		for(int tc=1;tc<=T;tc++) {
 			String VPS = br.readLine().trim();
+			
+			Stack<Character> stack = new Stack<>();
 			boolean flag = true;
-			for(int i=0;i<VPS.length();i++){
+			for(int i=0;i<VPS.length();i++) {
 				if(VPS.charAt(i) == '(') stack.push(VPS.charAt(i));
 				else {
-					if(stack.isEmpty() || stack.pop() != '(') {
+					if(stack.isEmpty() || stack.peek() == ')' ) {
 						flag = false;
 						break;
-					}
+					}else stack.pop();
 				}
 			}
-			
+
 			if(!stack.isEmpty()) flag = false;
 			
 			if(flag) System.out.println("YES");
