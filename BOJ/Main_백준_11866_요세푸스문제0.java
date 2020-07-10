d1aa14a992067bcb572115cf50d86d52a7281453
gksmfwm00 <gksmfwm00@naver.com>
2020-07-11 03:12:55 +0900

    반례

--- BOJ/Main_백준_11866_요세푸스문제0.java
+++ BOJ/Main_백준_11866_요세푸스문제0.java
@@ -1,33 +1,38 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_백준_11866_요세푸스문제0 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
+	
+		LinkedList<Integer> queue = new LinkedList<>();
+		for(int i=1;i<=N;i++) queue.offer(i);
 		
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i=1;i<=N;i++) queue.offer(i); 
-		
-		StringBuilder sb = new StringBuilder("<");
-		while(!queue.isEmpty()){
-			int idx = (K-1) < queue.size() ? (K-1) : (K-1) % queue.size();
-			
-			while(idx-- > 0){
-				int num = queue.poll();
-				queue.offer(num);
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		int idx = K - 1;
+		while(true) {
+			if(queue.size() == 1) {
+				sb.append(queue.get(idx) + ">");
+				queue.remove(idx);
+				
+				System.out.print(sb.toString());
+				
+				return;
+			}else {
+				sb.append(queue.get(idx) + ", ");
+				queue.remove(idx);
 			}
 			
-			if(queue.size() == 1) sb.append(queue.poll() + ">");
-			else sb.append(queue.poll() + ", ");
+			idx += K - 1;
+			
+			if(queue.size() <= idx) idx %= queue.size();
 		}
-		
-		System.out.print(sb.toString());
 	}
 }
