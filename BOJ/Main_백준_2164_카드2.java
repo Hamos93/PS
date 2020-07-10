3765298ae218ffd8ce785f4fc0be31d15c4e98f4
gksmfwm00 <gksmfwm00@naver.com>
2020-07-11 02:31:41 +0900

    Create 2164

--- BOJ/Main_백준_2164_카드2.java
+++ BOJ/Main_백준_2164_카드2.java
@@ -1,24 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main_백준_2164_카드2 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = sc.nextInt();
+		int N = Integer.parseInt(st.nextToken());
+		if(N == 1) {
+			System.out.print("1");
+			return;
+		}
 		
 		Queue<Integer> queue = new LinkedList<>();
-		for(int i=1;i<=N;i++)
-			queue.offer(i);
+		for(int i=1;i<=N;i++) queue.offer(i);
 		
-		while(true){
+		while(true) {
+			queue.poll();
+			
 			if(queue.size() == 1) {
 				System.out.print(queue.poll());
 				return;
 			}
-		
-			queue.poll();
+			
 			queue.offer(queue.poll());
 		}
 	}
