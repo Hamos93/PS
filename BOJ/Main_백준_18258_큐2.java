fb22627e84480f656f897665285e64b961599b5b
gksmfwm00 <gksmfwm00@naver.com>
2020-07-11 01:58:53 +0900

    Create 18258

--- BOJ/Main_백준_18258_큐2.java
+++ BOJ/Main_백준_18258_큐2.java
@@ -9,36 +9,37 @@ public class Main_백준_18258_큐2 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		int N = Integer.parseInt(st.nextToken());
-		
 		LinkedList<Integer> queue = new LinkedList<>();
+
 		StringBuilder sb = new StringBuilder();
-		while(N-- > 0){
+		while(0 < N--) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
 			String command = st.nextToken();
-			
-			if(command.equals("push")) queue.offer(Integer.parseInt(st.nextToken()));
-			
-			if(command.equals("pop")){
-				if(!queue.isEmpty()) sb.append(queue.poll() + "\n");
-				else sb.append("-1" + "\n");
-			}
-			
-			if(command.equals("size")) sb.append(queue.size() + "\n");
-
-			if(command.equals("empty")) {
-				if(queue.isEmpty()) sb.append("1" + "\n");
-				else sb.append("0" + "\n");
-			}
-			
-			if(command.equals("front")) {
-				if(!queue.isEmpty()) sb.append(queue.peekFirst() + "\n");
-				else sb.append("-1" + "\n");
-			}
-			
-			if(command.equals("back")) {
-				if(!queue.isEmpty()) sb.append(queue.peekLast() + "\n");
-				else sb.append("-1" + "\n");
+			switch (command) {
+			case "push":
+				int num = Integer.parseInt(st.nextToken());
+				queue.offer(num);
+				break;
+			case "pop":
+				if(queue.isEmpty()) sb.append("-1\n");
+				else sb.append(queue.poll() + "\n");
+				break;
+			case "size":
+				sb.append(queue.size() + "\n");
+				break;
+			case "empty":
+				if(queue.isEmpty()) sb.append("1\n");
+				else sb.append("0\n");
+				break;
+			case "front":
+				if(queue.isEmpty()) sb.append("-1\n");
+				else sb.append(queue.getFirst() + "\n");
+				break;
+			case "back":
+				if(queue.isEmpty()) sb.append("-1\n");
+				else sb.append(queue.getLast() + "\n");
+				break;
 			}
 		}
 		
