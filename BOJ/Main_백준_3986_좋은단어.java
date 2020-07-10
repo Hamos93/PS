74ccb3caf99a4da54dc2e74ad557d23ce106f333
gksmfwm00 <gksmfwm00@naver.com>
2020-07-10 17:01:32 +0900

    Create 3986

--- BOJ/Main_백준_3986_좋은단어.java
+++ BOJ/Main_백준_3986_좋은단어.java
@@ -9,17 +9,20 @@ public class Main_백준_3986_좋은단어 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		int N = Integer.parseInt(st.nextToken());
+		
 		int answer = 0;
-		while(N-- > 0){
-			Stack<Character> stack = new Stack<>();
+		while(0 < N--) {
+			String str = br.readLine().trim();
 			
-			String word = br.readLine().trim();
-
-			for(int i=0;i<word.length();i++){
-				char ch = word.charAt(i);
+			Stack<Character> stack = new Stack<>();
+			for(int i=0;i<str.length();i++) {
+				char ch = str.charAt(i);
 				
-				if(stack.isEmpty() || stack.peek() != ch) stack.push(ch);
-				else if(stack.peek() == ch) stack.pop();
+				if(stack.isEmpty()) stack.push(ch);
+				else {
+					if(stack.peek() == ch) stack.pop();
+					else stack.push(ch);
+				}
 			}
 			
 			if(stack.isEmpty()) answer++;
