ed5d13b2b9a52d38585b009b1224b25668f811b2
gksmfwm00 <gksmfwm00@naver.com>
2020-07-11 02:22:18 +0900

    Create 10866

--- BOJ/Main_백준_10866_덱.java
+++ BOJ/Main_백준_10866_덱.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -9,44 +8,47 @@ public class Main_백준_10866_덱 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		Deque<Integer> queue = new LinkedList<>();
-
 		int N = Integer.parseInt(st.nextToken());
-		while(N-- > 0){
+		LinkedList<Integer> deque = new LinkedList<>();
+
+		StringBuilder sb = new StringBuilder();
+		while(0 < N--) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
 			String command = st.nextToken();
-			
-			if(command.equals("push_front")) queue.offerFirst(Integer.parseInt(st.nextToken()));
-			
-			if(command.equals("push_back")) queue.offerLast(Integer.parseInt(st.nextToken()));
-			
-			if(command.equals("pop_front")) {
-				if(!queue.isEmpty()) System.out.println(queue.pollFirst());
-				else System.out.println("-1");
-			}
-			
-			if(command.equals("pop_back")) {
-				if(!queue.isEmpty()) System.out.println(queue.pollLast());
-				else System.out.println("-1");
-			}
-			
-			if(command.equals("size")) System.out.println(queue.size());
-		
-			if(command.equals("empty")) {
-				if(!queue.isEmpty()) System.out.println("0");
-				else System.out.println("1");
-			}
-			
-			if(command.equals("front")) {
-				if(!queue.isEmpty()) System.out.println(queue.peekFirst());
-				else System.out.println("-1");
-			}
-			
-			if(command.equals("back")) {
-				if(!queue.isEmpty()) System.out.println(queue.peekLast());
-				else System.out.println("-1");
+			switch (command) {
+			case "push_front":
+				deque.addFirst(Integer.parseInt(st.nextToken()));
+				break;
+			case "push_back":
+				deque.addLast(Integer.parseInt(st.nextToken()));
+				break;
+			case "pop_front":
+				if(deque.isEmpty()) sb.append("-1\n");
+				else sb.append(deque.pollFirst() + "\n");
+				break;
+			case "pop_back":
+				if(deque.isEmpty()) sb.append("-1\n");
+				else sb.append(deque.pollLast() + "\n");
+				break;
+			case "size":
+				sb.append(deque.size() + "\n");
+				break;
+			case "empty":
+				if(deque.isEmpty()) sb.append("1\n");
+				else sb.append("0\n");
+				break;
+			case "front":
+				if(deque.isEmpty()) sb.append("-1\n");
+				else sb.append(deque.getFirst() + "\n");
+				break;
+			case "back":
+				if(deque.isEmpty()) sb.append("-1\n");
+				else sb.append(deque.getLast() + "\n");
+				break;
 			}
 		}
+		
+		System.out.print(sb.toString());
 	}
 }
