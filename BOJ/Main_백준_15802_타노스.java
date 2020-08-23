be3912a032a64fe44e47e5851a8d15d588ba4d88
gksmfwm00 <gksmfwm00@naver.com>
2020-08-24 03:43:03 +0900

    Create 15802

---
+++ BOJ/Main_백준_15802_타노스.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main_백준_15802_타노스 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		System.out.print("1");
+	}
+}
