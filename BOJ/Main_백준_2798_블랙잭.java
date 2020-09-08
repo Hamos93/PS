41c6d2afb5c722641499412348b4193abbbdc041
gksmfwm00 <gksmfwm00@naver.com>
2020-09-09 04:13:16 +0900

    브루트포스 반복문

--- BOJ/Main_백준_2798_블랙잭.java
+++ BOJ/Main_백준_2798_블랙잭.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_백준_2798_블랙잭_1 {
+public class Main_백준_2798_블랙잭 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
