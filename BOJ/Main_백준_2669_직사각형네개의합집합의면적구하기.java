0b460fbb87e0670299e897e6937ca7089fcaa145
gksmfwm00 <gksmfwm00@naver.com>
2020-09-11 10:53:54 +0900

    구현

--- BOJ/Main_백준_2669_직사각형네개의합집합의면적구하기.java
+++ BOJ/Main_백준_2669_직사각형네개의합집합의면적구하기.java
@@ -7,29 +7,25 @@ public class Main_백준_2669_직사각형네개의합집합의면적구하기 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
-		int[][] map = new int[101][101];
-		int input = 4;
-		
-		while(input-- > 0){
+		int[][] area = new int[101][101];
+		for(int i=1;i<=4;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			
-			int x1 = Integer.parseInt(st.nextToken()); 
-			int y1 = Integer.parseInt(st.nextToken()); 
-			int x2 = Integer.parseInt(st.nextToken()); 
-			int y2 = Integer.parseInt(st.nextToken()); 
 		
-			for(int i=x1;i<x2;i++){
-				for(int j=y1;j<y2;j++)
-					map[i][j] = 1;
+			int x1 = Integer.parseInt(st.nextToken()); int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken()); int y2 = Integer.parseInt(st.nextToken());
+		
+			for(int x=x1;x<x2;x++) {
+				for(int y=y1;y<y2;y++)
+					area[x][y] = 1;
 			}
 		}
 		
-		int area = 0;
-		for(int i=1;i<=100;i++){
+		int result = 0;
+		for(int i=1;i<=100;i++) {
 			for(int j=1;j<=100;j++)
-				if(map[i][j] == 1) area++;
+				if(area[i][j] != 0) result++; 
 		}
 		
-		System.out.print(area);
+		System.out.print(result);
 	}
 }
