f7626f9dc160692e71845fbd746ee5b7a8c997ac
gksmfwm00 <gksmfwm00@naver.com>
2020-10-09 07:47:35 +0900

    그리디

---
+++ BOJ/Main_백준_4796_캠핑.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_4796_캠핑 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int T = 1;
+		while(true) {
+			st = new StringTokenizer(br.readLine(), " ");
+		
+			int L = Integer.parseInt(st.nextToken());
+			int P = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken());
+		
+			if(L == 0 && P == 0 && V == 0) break;
+			
+			int day = V / P * L;
+			int remainder = L < (V % P) ? L : V % P;
+			
+			System.out.println("Case " + T++ + ": " + (day + remainder));
+		}
+	}
+}
