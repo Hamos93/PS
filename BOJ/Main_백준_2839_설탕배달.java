7e34075e3ddfda0f6572d9ece16216552a2c7d9d
gksmfwm00 <gksmfwm00@naver.com>
2020-10-11 01:09:27 +0900

    그리디

--- BOJ/Main_백준_2839_설탕배달.java
+++ BOJ/Main_백준_2839_설탕배달.java
@@ -6,29 +6,32 @@ public class Main_백준_2839_설탕배달 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+		
 		int N = Integer.parseInt(st.nextToken());
-		int result = Integer.MAX_VALUE;
-		for(int i=1;i<=N;i++) {
-			if(5 * i == N) {
-				int total = i;
-				result = total < result ? total : result;
+		
+		int cnt = Integer.MAX_VALUE;
+		if(N % 5 == 0) cnt = Math.min(cnt, N / 5);
+		if(N % 3 == 0) cnt = Math.min(cnt, N / 3);
+		
+		int idx = 1;
+		while(true) {
+			int sugar = N;
+			int bag5 = idx;
+			
+			sugar -= (5 * bag5);
+			
+			if(sugar <= 0) break;
+			
+			if(sugar % 3 == 0) {
+				int bag3 = sugar / 3;
+			
+				cnt = Math.min(cnt, bag5 + bag3);
 			}
-
-			for(int j=1;j<=N;j++) {
-				if(3 * j == N) {
-					int total = j;
-					result = total < result ? total : result;
-				}
-				
-				if(5 * i + 3 * j == N) {
-					int total = i + j;
-					result = total < result ? total : result;
-				}
-			}
+			
+			idx++;
 		}
 		
-		if(result == Integer.MAX_VALUE) System.out.print("-1");
-		else System.out.print(result);
+		if(cnt == Integer.MAX_VALUE) System.out.print("-1");
+		else System.out.print(cnt);
 	}
 }
