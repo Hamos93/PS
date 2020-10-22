5c9bf9b994a1d73b8db3d613a3567a2a20e2a2c9
gksmfwm00 <gksmfwm00@naver.com>
2020-10-22 23:39:51 +0900

    최댓값과 최솟값

---
+++ Programmers/Level2/Solution_최댓값과최솟값.java
@@ -0,0 +1,23 @@
+
+public class Solution_최댓값과최솟값 {
+	public static String solution(String s) {
+		String[] array = s.split(" ");
+
+		int max = Integer.parseInt(array[0]);
+		int min = Integer.parseInt(array[0]);
+		
+		for(int i=0;i<array.length;i++) {
+			int value = Integer.parseInt(array[i]);
+			
+			max = max < value ? value : max;
+			min = value < min ? value : min;
+		}
+		
+		String answer = min + " " + max;
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		System.out.print(solution(solution("1 2 3 4")));
+	}
+}
