5e03e94fd88ef8ae200309f23da1a92e53012af9
gksmfwm00 <gksmfwm00@naver.com>
2020-11-09 05:14:17 +0900

    BFS

--- BOJ/Main_백준_16953_A화살표B.java
+++ BOJ/Main_백준_16953_A화살표B.java
@@ -7,39 +7,34 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main_백준_16953_A화살표B {
-	private static long answer;
-	
 	public static void bfs(long A, long B) {
 		Queue<long[]> queue = new LinkedList<>();
-		Set<String> hashSet = new HashSet<>();
+		Set<Long> hashSet = new HashSet<>();
 		
 		queue.offer(new long[] { A, 1 });
-		hashSet.add(Long.toString(A));
+		hashSet.add(A);
 		
 		while(!queue.isEmpty()) {
 			long[] poll = queue.poll();
 			long value = poll[0], cnt = poll[1];
 			
 			if(value == B) {
-				answer = cnt;
+				System.out.print(cnt);
 				return;
 			}
 
-			String str = Long.toString(value * 2);
-			if(value * 2 <= B && !hashSet.contains(str)) {
+			if(value * 2 <= B && !hashSet.contains(value * 2)) {
 				queue.offer(new long[] { value * 2, cnt + 1 });
-				hashSet.add(str);
+				hashSet.add(value * 2);
 			}
 			
-			str = Long.toString(value);
-			str += 1;
-			
-			long temp = Long.parseLong(str);
-			if(temp <= B && !hashSet.contains(str)) {
-				queue.offer(new long[] { temp, cnt + 1 });
-				hashSet.add(str);
+			if((value * 10 + 1) <= B && !hashSet.contains(value * 10 + 1)) {
+				queue.offer(new long[] { value * 10 + 1, cnt + 1 });
+				hashSet.add(value * 10 + 1);
 			}
 		}
+		
+		System.out.print("-1");
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -49,9 +44,6 @@ public class Main_백준_16953_A화살표B {
 		long A = Long.parseLong(st.nextToken());
 		long B = Long.parseLong(st.nextToken());
 
-		answer = -1;
 		bfs(A, B);
-		
-		System.out.print(answer);
 	}
 }
