818756b6075d857a41ec1e9c177b7aa7a0441f01
gksmfwm00 <gksmfwm00@naver.com>
2020-11-16 08:34:33 +0900

    Update

--- BOJ/Main_백준_1072_게임.java
+++ BOJ/Main_백준_1072_게임.java
@@ -1,34 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_백준_1072_게임 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		while(true) {
-			String line = br.readLine();
-			
-			if(line == null || line.length() == 0) break;
-			
-			String[] input = line.split(" ");
-			
-			long X = Long.parseLong(input[0]);
-			long Y = Long.parseLong(input[1]);
-			long Z = (Y * 100) / X;
+		long X = Long.parseLong(st.nextToken());
+		long Y = Long.parseLong(st.nextToken());
+		long Z = (Y * 100) / X;
 
-			long low = 1, high = Integer.MAX_VALUE;
-			long result = -1;
-			while(low <= high) {
-				long mid = (low + high) / 2;
-				long value = ((Y + mid) * 100) / (X + mid);
+		long low = 1, high = Integer.MAX_VALUE;
+		long result = -1;
+		while(low <= high) {
+			long mid = (low + high) / 2;
+			long value = ((Y + mid) * 100) / (X + mid);
 
-				if(Z < value) {
-					result = mid;
-					high = mid - 1;
-				}else low = mid + 1;
-			}
-
-			System.out.println(result);
+			if(Z < value) {
+				result = mid;
+				high = mid - 1;
+			}else low = mid + 1;
 		}
+
+		System.out.print(result);
 	}
 }
