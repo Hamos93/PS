e7ba1279129c54d5b678dc1c49c5625b71a6ed15
Hamos93 <gksmfwm00@naver.com>
2021-03-15 17:24:40 +0900

    DFS

--- BOJ/Main_백준_17073_나무위의빗물.java
+++ BOJ/Main_백준_17073_나무위의빗물.java
@@ -1,29 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_백준_17073_나무위의빗물 {
+	private static int N, cnt;
+	private static ArrayList<Integer>[] adjList;
+	private static boolean[] visit;
+	
+	public static void dfs(int x) {
+		visit[x] = true;
+		
+		boolean flag = true;
+		for(int next:adjList[x]) {
+			if(!visit[next]) {
+				flag = false;
+				dfs(next);
+			}
+		}
+		
+		if(flag) cnt++;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		int W = Integer.parseInt(st.nextToken());
 		
-		int[] tree = new int[N + 1];
+		adjList = new ArrayList[N + 1];
+		for(int i=1;i<=N;i++)
+			adjList[i] = new ArrayList<>();
+		
 		for(int i=1;i<=N-1;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			
-			tree[u]++; tree[v]++;
+
+			adjList[u].add(v); adjList[v].add(u);
 		}
-	
-		int cnt = 0;
-		for(int i=2;i<=N;i++)
-			if(tree[i] == 1) 
-				cnt++;
+
+		cnt = 0;
+
+		visit = new boolean[N + 1];
+		dfs(1);
 		
 		double P = (double)(W) / cnt;
 		System.out.print(String.format("%.10f", P));
