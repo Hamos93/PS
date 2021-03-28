34bf57ce3b67c371f1fdb83741896076dc325607
Hamos93 <gksmfwm00@naver.com>
2021-03-28 23:11:09 +0900

    DP

---
+++ SWEA/D3/Solution_D3_5215_햄버거다이어트.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D3_5215_햄버거다이어트 {
+	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int TC = Integer.parseInt(st.nextToken());
+		for(int tc=1;tc<=TC;tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int N = Integer.parseInt(st.nextToken());
+			int L = Integer.parseInt(st.nextToken());
+		
+			int[][] dp = new int[N + 1][L + 1];
+			
+			for(int i=1;i<=N;i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				
+				int T = Integer.parseInt(st.nextToken());
+				int K = Integer.parseInt(st.nextToken());
+				
+				for(int j=1;j<=L;j++) {
+					if(j < K) dp[i][j] = dp[i-1][j-1];
+					else dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j - K] + T);
+				}
+			}
+		
+			sb.append("#" + tc + " " + dp[N][L] + "\n");
+		}
+		
+		System.out.print(sb.toString().trim());
+	}
+}
