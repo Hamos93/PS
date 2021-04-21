d1d233f3db1c41b112f07f33805986c6bc433df2
Hamos93 <gksmfwm00@naver.com>
2021-04-21 19:18:13 +0900

    DP

---
+++ Programmers/Level3/Solution_L3_2xn타일링.java
@@ -0,0 +1,16 @@
+
+public class Solution_L3_2xn타일링 {
+	public static int solution(int n) {
+		int[] dp = new int[60001];
+		dp[1] = 1; dp[2] = 2;
+		
+		for(int i=3;i<=n;i++)
+			dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+		
+		return dp[n];
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(solution(60000));
+	}
+}
