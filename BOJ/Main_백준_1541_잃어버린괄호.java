972fe9a0cae08482ea19c11988d2729d1fd18759
Hamos93 <gksmfwm00@naver.com>
2021-06-30 15:27:48 +0900

    그리디

--- BOJ/Main_백준_1541_잃어버린괄호.java
+++ BOJ/Main_백준_1541_잃어버린괄호.java
@@ -2,27 +2,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main_백준_1541_잃어버린괄호 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] formula = br.readLine().split("-");
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("-");
 
-		int len = formula.length;
-		for(int i=0;i<len;i++) {
-			int value = 0;
-			if(formula[i].contains("+")) {
-				// "+" 를 기준으로 split
-				String[] temp = formula[i].split("\\+");
-				for(int k=0;k<temp.length;k++)
-					value += Integer.parseInt(temp[k]);
-			
-				formula[i] = Integer.toString(value);
-			}
-		}
+        int result = 0;
+        for(int i=0;i<input.length;i++){
+            String[] expression = input[i].split("\\+");
 
-		int result = Integer.parseInt(formula[0]);
-		for(int i=1;i<len;i++)
-			result -= Integer.parseInt(formula[i]);
+            int value = 0;
+            for(String str : expression)
+                value += Integer.parseInt(str);
 
-		System.out.print(result);
-	}
+            if(i == 0) {
+                result += value;
+                continue;
+            }
+
+            result -= value;
+        }
+
+        System.out.print(result);
+    }
 }
--- BOJ/Main_백준_2217_로프.java
+++ BOJ/Main_백준_2217_로프.java
@@ -1,34 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_백준_2217_로프 {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
 
-		ArrayList<Integer> rope = new ArrayList<>(); 
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-			int value = Integer.parseInt(st.nextToken());
-			rope.add(value);
-		}
+        Arrays.sort(arr);
 
-		Collections.sort(rope, Collections.reverseOrder());
-		
-		int maxWeight = 0, cnt = 1, size = rope.size();
-		for(int i=0;i<size;i++) {
-			int weight = rope.get(i) * cnt;
-			cnt++;
-			
-			maxWeight = maxWeight < weight ? weight : maxWeight;
-		}
-		
-		System.out.print(maxWeight);
-	}
+        int w = 0, k = 1;
+        for(int i=N-1;0<=i;i--){
+            int weight = k * arr[i];
+
+            w = Math.max(w, weight);
+            k++;
+        }
+
+        System.out.print(w);
+    }
 }
